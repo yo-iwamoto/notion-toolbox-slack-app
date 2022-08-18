5cd71a9065f4ea5706e7ff2fa31f53ff4adb8826
Yo Iwamoto <you.iwamoto918@gmail.com>
2022-08-18 20:04:42 +0900

    impl: ng word police

--- functions/src/config/env.ts
+++ functions/src/config/env.ts
@@ -6,5 +6,6 @@ if (process.env.NODE_ENV === 'development') {
 
 export const NOTION_INTEGRATION_TOKEN = process.env.NOTION_INTEGRATION_TOKEN as string;
 export const NOTION_QUICK_DB_ID = process.env.NOTION_QUICK_DB_ID as string;
+export const NOTION_WORDS_DB_ID = process.env.NOTION_WORDS_DB_ID as string;
 export const SLACK_SIGNING_SECRET = process.env.SLACK_SIGNING_SECRET as string;
 export const SLACK_BOT_TOKEN = process.env.SLACK_BOT_TOKEN as string;
