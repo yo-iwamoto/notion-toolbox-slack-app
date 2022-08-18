a08dbd82c1ba0700b62d8cf6cced1dd5cb1a2643
Yo Iwamoto <you.iwamoto918@gmail.com>
2022-08-18 19:16:02 +0900

    fix: config

--- functions/scripts/deploy.sh
+++ functions/scripts/deploy.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-gcloud functions deploy handleSlack --runtime=nodejs16 --trigger-http
+gcloud functions deploy handleSlack --runtime=nodejs16 --trigger-http --project=slack-notion-toolbox
